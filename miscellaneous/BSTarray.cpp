5a332c9572bdee9d10050ada7472a8c6fddba93e
new-Rodin <shafqatjabbar2911@gmail.com>
2025-09-13 12:18:41 +0000

    Created a miscellaneous folder to dump all the extra programs.

---
+++ miscellaneous/BSTarray.cpp
@@ -0,0 +1,43 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n,i,c=0;
+    cout<<"Enter the number of nodes:"<<endl;
+    cin>>n;
+    vector <int> a(1000,-1); // All the vector values i.e the nodes are initialized with -1 to make it seem as if they are all empty and ready to be filled.
+    int v[n];
+    cout<<"Enter "<<n<<" values in the nodes:"<<endl; // First we store all the numbers in a simple array.
+    for(i=0;i<n;i++)
+    cin>>v[i];
+    i=0;
+    while(true)
+    {
+        if(c==n) //Checks if we have reached the end of the array v[i]
+        break;
+        if(a[i]!=-1)
+        {
+            if(v[c]<a[i]) //Traverses to the left if the element is smaller
+            i = 2*i+1;
+            else// Traverses to the right if the element is larger
+            i = 2*i+2;
+        }
+        else{
+        a[i]=v[c]; //If we arrive at a node that is empty then that is the point where we store them. 
+        i=0;
+        c++;
+        }
+    }
+    c=0;
+    i=0;
+    cout<<"The elements stored are:"<<endl;
+    while(true)
+    {
+        if(c==n)
+        break;
+        cout<<a[i]<<"  "; //During the printing of the values -1 depicts a void at the index i i.e it means there is no node present at that index.
+        if(a[i]!=-1)
+        c++;
+        i++;
+    }
+}
